00d1900dba12c242f85561795c9d92fd1189b9be
Linggabot <94733123+Linggabot@users.noreply.github.com>
2021-11-22 14:09:59 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -4,24 +4,24 @@ return `PAYMENT BY ZEEONE OFC
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ gak ada kak
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ gak ada kak
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ gak ada kak
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ gak ada kak
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ gak ada kak
+2. PULSA
+	ㅁ 081477094099
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 081477094099
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama HE*****O
 
 *📲 PULSA*
 
